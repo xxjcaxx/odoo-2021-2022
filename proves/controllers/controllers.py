6bcfe3e00ec38ba85df051a937ed560a839c3adf
xxjcaxx <jocasal@gmail.com>
2021-10-26 17:55:11 +0000

    dimarts 26

--- proves/controllers/controllers.py
+++ proves/controllers/controllers.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+import json
+from odoo import tools
 
 
-# class Proves(http.Controller):
-#     @http.route('/proves/proves/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Proves_controller(http.Controller):
+     @http.route('/proves/productes/', auth="none", cors='*', csrf=False, type='http')
+     def get_productes(self, **kw):
+         productes = http.request.env['product.product'].sudo().search([])
+         print(productes)
+        # return json.dumps(productes.read(['id','name','image_1920']))
+
+         return http.Response(
+            json.dumps(productes.read(['id','name','image_1920']), default=tools.date_utils.json_default),
+            status=200,
+            mimetype='application/json'
+           )
 
 #     @http.route('/proves/proves/objects/', auth='public')
 #     def list(self, **kw):
